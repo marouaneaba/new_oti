2cf0c7350ddc01ac5ea3e6b936be50e1bd0c7375
marouaneaba <mar.abakarim@gmail.com>
2017-12-08 02:31:16 +0100

    modification les test des alerts

--- test/alert_test.js
+++ test/alert_test.js
@@ -1,67 +1,339 @@
-QUnit.module("alertt", {
-//	setup:function(){alert("setup moneyOps individual test");},
-//	teardown:function(){alert("teardown moneyOps individual test");}
+QUnit.module("money", {
+//	setup:function(){alert("setup money");},
+//	teardown:function(){alert("teardown money");}
 });
 
 QUnit.test("test_computeresults", function(assert)
 {
         var fixture="";
         fixture+=("<form id='form0'>");
-        fixture+=("<input type='text' id='v1' name='v1' value='-2'/>");
+        fixture+=("<input type='text' id='v1' name='v1' value='2'/>");
         fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
         fixture+=("<input type='text' id='v2' name='v2' value='5'/>");
         fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
         fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
         fixture+=("</form>");
 
-
+        domVar = sinon.spy(window,"alert");
         var fixtureNode=document.getElementById("qunit-fixture");
         fixtureNode.innerHTML=fixture;
 
 
-        var m1EUR=new money(1,"EUR");
-      	var m1eur=new money(1,"eur");
-
-        var m = MoneyOps.add(m1EUR,m1eur);
-
-        sinon.spy(window,"alert");
+        
         assert.ok(!window.alert.calledOnce);
-
+        domVar.restore();
 }
 );
 QUnit.test("test add different currency", function(assert)
 {
-        var fixture="";
+         var fixture="";
         fixture+=("<form id='form0'>");
-        fixture+=("<input type='text' id='v1' name='v1' value='-2'/>");
+        fixture+=("<input type='text' id='v1' name='v1' value='19'/>");
         fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
-        fixture+=("<input type='text' id='v2' name='v2' value='5'/>");
-        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='CHF'/>");
         fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
         fixture+=("</form>");
 
-
+        var r =sinon.spy(window,"alert");
         var fixtureNode=document.getElementById("qunit-fixture");
         fixtureNode.innerHTML=fixture;
 
 
-        var m1EUR=new money(1,"CHF");
-      	var m1eur=new money(1,"eur");
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
 
-
-        var m = MoneyOps.add(m1EUR,m1eur);
-
-        sinon.spy(window,"alert");
-        assert.ok(true);
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
 
 }
 );
-/*money Ops*/
-// monye null
-//value <0
-// value1 = 0 and value2 =0
-//currencu1 != currency2
+QUnit.test("test sub different currency", function(assert)
+{
+         var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='19'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='CHF'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='SUB'/>");
+        fixture+=("</form>");
 
-/* creation money */
-//v == null ou curency == null ou value <0
-//currency >3
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test currency.length > 3", function(assert)
+{
+         var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='19'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EURR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='SUB'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test null value", function(assert)
+{
+         var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value=''/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='SUB'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test null currency", function(assert)
+{
+         var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='19'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value=''/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='CHF'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test null money", function(assert)
+{
+         var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value=''/>");
+        fixture+=("<input type='text' id='c1' name='c1' value=''/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='CHF'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test ADD value  < 0", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='-19'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test SUB value  < 0", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='-19'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='SUB'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test Sustraction m1 < m2", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='2'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='SUB'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test money1 = 0  and money2 = 0", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='0'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='0'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(window.alert.calledOnce);
+        r.restore();
+
+}
+);
+/**/
+QUnit.test("test Add Money currency.length > 3  and value > 0", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='2'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='8'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(!window.alert.calledOnce);
+        r.restore();
+
+}
+);
+QUnit.test("test SUB Money currency.length > 3  and value > 0", function(assert)
+{
+        var fixture="";
+        fixture+=("<form id='form0'>");
+        fixture+=("<input type='text' id='v1' name='v1' value='6'/>");
+        fixture+=("<input type='text' id='c1' name='c1' value='EUR'/>");
+        fixture+=("<input type='text' id='v2' name='v2' value='2'/>");
+        fixture+=("<input type='text' id='c2' name='c2' value='EUR'/>");
+        fixture+=("<input type='text' id='ops' name='ops' value='ADD'/>");
+        fixture+=("</form>");
+
+        var r =sinon.spy(window,"alert");
+        var fixtureNode=document.getElementById("qunit-fixture");
+        fixtureNode.innerHTML=fixture;
+
+
+        var c=new calc();
+        c.computeResult(document.getElementById('form0'));
+
+        
+        
+        assert.ok(!window.alert.calledOnce);
+        r.restore();
+
+}
+);
